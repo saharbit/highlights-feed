3047ba955a46da54d1eea95ce65334d6a72ce525
Sahar <sahar.bit@gmail.com>
2021-04-28 20:16:42 +0300

    Boxes icon

--- components/Header.js
+++ components/Header.js
@@ -2,20 +2,21 @@ import React, { useState } from "react";
 import Modal from "react-modal";
 import Subreddits from "./Subreddits";
 import SubredditsModal from "./SubredditsModal";
+import BoxesIcon from "../icons/BoxesIcon";
 
 const Header = ({ tab: { label, Icon }, subreddits, setSubreddits }) => {
   const [isSubredditsModalOpen, setIsSubredditsModalOpen] = useState(false);
   return (
-    <div className="flex justify-between px-2 py-3">
+    <div className="flex justify-between items-center px-2 py-3">
       <div className="flex items-center">
         <Icon className="w-6 h-6 mr-2" />
         <div className="font-bold text-xl">{label}</div>
       </div>
       <div
         onClick={() => setIsSubredditsModalOpen(true)}
-        className="w-6 md:hidden cursor-pointer"
+        className="md:hidden cursor-pointer"
       >
-        <img src="/adjustments.svg" alt="Adjust" />
+        <BoxesIcon />
       </div>
       <SubredditsModal
         isOpen={isSubredditsModalOpen}
