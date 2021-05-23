eb0662d53beb4a80b66fff5a996a6fdc6308fdc5
Sahar <sahar.bit@gmail.com>
2021-05-23 18:05:54 +0300

    Fix z-Index

--- components/MobileComponents/MobileHeader.js
+++ components/MobileComponents/MobileHeader.js
@@ -6,7 +6,7 @@ const MobileHeader = () => {
   const { label, Icon } = useSelector(getCurrentTab);
 
   return (
-    <div className="flex justify-between items-center px-2 h-12 border-b fixed inset-x-0 top-0 z-10 bg-white md:hidden">
+    <div className="flex justify-between items-center px-2 h-12 border-b fixed inset-x-0 top-0 z-20 bg-white md:hidden">
       <div className="flex items-center">
         <Icon className="w-6 h-6 mr-2" />
         <div className="font-bold text-xl">{label}</div>
